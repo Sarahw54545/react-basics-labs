ae085500bd5ce6dcde903153c60ca796eaae3ac7
Sarah Walsh <20102911@mail.wit.ie>
2025-01-29 16:07:56 +0000

    Add task: form added

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -1,28 +1,29 @@
 import React, { useState } from 'react';
 import './App.css';
 import Task from './components/Task';
+import AddTaskForm from './components/Form';
 
 function App() {
-  const [ taskState, setTaskState ] = useState({
+  const [taskState, setTaskState] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", priority: "Low", done: false},
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "Medium", done: false},
-      { id: 3, title: "Tidy up", description: "Tidy room", deadline: "Today", priority: "High", done: false}
+      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today", priority: "Low", done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "Medium", done: false },
+      { id: 3, title: "Tidy up", description: "Tidy room", deadline: "Today", priority: "High", done: false }
     ]
   });
 
   const doneHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
     tasks[taskIndex].done = !tasks[taskIndex].done;
-    setTaskState({tasks});
+    setTaskState({ tasks });
     // console.log(`${taskIndex} ${tasks[taskIndex].done}`);
   }
 
   const deleteHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
     tasks.splice(taskIndex, 1);
-    setTaskState({tasks});
-  } 
+    setTaskState({ tasks });
+  }
 
   return (
     <div className="container">
@@ -36,9 +37,11 @@ function App() {
           key={task.id}
           done={task.done}
           markDone={() => doneHandler(index)}
-          deleteTask = {() => deleteHandler(index)}
+          deleteTask={() => deleteHandler(index)}
         />
       ))}
+
+      <AddTaskForm />
     </div>
   );
 }
