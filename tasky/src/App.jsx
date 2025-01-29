701fa9a454a0bac277127befd77a79b8686c0b3f
Sarah Walsh <20102911@mail.wit.ie>
2025-01-29 10:40:58 +0000

    Added keys to tasks

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -3,13 +3,14 @@ import './App.css';
 import Task from './components/Task';
 
 function App() {
-  const [taskState, setTaskState] = useState({
+  const [ taskState, setTaskState ] = useState({
     tasks: [
-      { title: "Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { title: "Tidy up", deadline: "Today" }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { id: 3, title: "Tidy up", description: "Tidy room", deadline: "Today" }
     ]
   });
+
   return (
     <div className="container">
       <h1>Tasky</h1>
@@ -18,6 +19,7 @@ function App() {
           title={task.title}
           description={task.description}
           deadline={task.deadline}
+          key={task.id}
         />
       ))}
     </div>
