246a1d0c930c43656a0a49b11d18a8cd5ced9dce
Sarah Walsh <20102911@mail.wit.ie>
2025-01-29 22:41:22 +0000

    Tasky Lab 2b - Exercises Done

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -8,7 +8,7 @@ const Task = (props) => {
             <p>Due: {props.deadline}</p>
             <p className="description">{props.description}</p>
             <p className="priority" style={{backgroundColor: props.priority === "Low" ? 'green' : props.priority === "Medium" ? 'orange' : "red"}}>{props.priority}</p>
-            <button onClick={props.markDone} className="doneButton">Done</button>
+            <button className="doneButton" onClick={props.markDone}>Done</button>
             <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
         </div>
     )
